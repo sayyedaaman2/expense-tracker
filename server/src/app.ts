74fdf060a17672710629f99839090de6f5d1d31f
Sayyed Aaman <sayyedaaman9@gmail.com>
2025-09-19 20:22:29 +0530

    added middlewares notFound logger errorhandler

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,32 @@
 import express from 'express';
 import type {Application} from 'express';
+import helmet from 'helmet';
 import cors from 'cors';
+import morgan from 'morgan';
+
+import {errorHandler,logger,notFound} from './middlewares'
 import routes from './routes';
 const app:Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
+app.use(helmet());
 app.use(cors());
+app.use(morgan("dev"));
+
+// Custom Logger
+app.use(logger);
+
 
 
 app.get('/', (req,res)=>{
     res.send("Expense Tracker API is running 🚀")
 })
 app.use('/api',routes);
+
+
+app.use(notFound)
+app.use(errorHandler)
 
 export default app;
